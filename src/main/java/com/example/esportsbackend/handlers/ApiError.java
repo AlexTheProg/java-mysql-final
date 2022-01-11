d7fed2f4b62eed2b2587de94be8d92b348f14110
Alexandru Contu <alexandru.contu99@gmail.com>
2022-01-12 00:32:30 +0200

    refactor and have all handlers in one single class

--- src/main/java/com/example/esportsbackend/handlers/ApiError.java
+++ src/main/java/com/example/esportsbackend/handlers/ApiError.java
@@ -5,19 +5,19 @@ import org.springframework.http.HttpStatus;
 import java.util.Arrays;
 import java.util.List;
 
-public class ApiException {
+public class ApiError {
     private HttpStatus status;
     private String message;
     private List<String> errors;
 
-    public ApiException(HttpStatus status, String message, List<String> errors) {
+    public ApiError(HttpStatus status, String message, List<String> errors) {
         super();
         this.status = status;
         this.message = message;
         this.errors = errors;
     }
 
-    public ApiException(HttpStatus status, String message, String error) {
+    public ApiError(HttpStatus status, String message, String error) {
         super();
         this.status = status;
         this.message = message;
